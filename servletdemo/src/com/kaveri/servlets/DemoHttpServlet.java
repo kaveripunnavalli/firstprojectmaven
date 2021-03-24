adc35772d0100692026dbe754aa0e966ddf70bce
kaveripunnavalli <80825591+kaveripunnavalli@users.noreply.github.com>
2021-03-24 10:04:07 +0530

    Add files via upload

---
+++ servletdemo/src/com/kaveri/servlets/DemoHttpServlet.java
@@ -0,0 +1,49 @@
+package com.kaveri.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DemoHttpServlet
+ */
+@WebServlet("/DemoHttpServlet")
+public class DemoHttpServlet extends HttpServlet 
+{
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DemoHttpServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter pw=response.getWriter();
+		pw.println("<h1>Welcome</h1>");
+		String name=request.getParameter("name");
+		pw.print("<h1>"+name+"</h1>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
